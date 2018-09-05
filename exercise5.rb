cace7ed0ef5329909a3b16a588d8a087bc3100fc
Ndelforno <nicolas.delforno@gmail.com>
2018-09-05 16:12:41 -0400

    exercise 5 done

---
+++ exercise5.rb
@@ -0,0 +1,8 @@
+def greet_backwards(name)
+  return "Hello, #{name.reverse * 2}! Welcome home."
+end
+
+puts greet_backwards("Bob")
+puts greet_backwards("Shirly")
+puts greet_backwards("Sue")
+puts greet_backwards("Andy")
